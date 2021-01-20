223c3332f8440d14fde9555c006d5f5ebd8fdc8d
rhyslwells101 <rhyslwells@outlook.com>
2021-01-20 13:04:25 +0000

    made progress on input in main file. Working on how to define functions on djpair.

--- MainDoc.py
+++ MainDoc.py
@@ -1,6 +1,6 @@
 #input code from roam even if it is wrong. Put all of in here .
 import math
-import numpy as np
+import numpy as np # for array orientation, homogeneous terms. Faster than Python.
 import matplotlib as plt
 import pandas as pd
 import more_itertools as iter
@@ -9,7 +9,8 @@ from itertools import permutations
 from itertools import chain, combinations
 from more_itertools import powerset,set_partitions
 
-
+n=3
+nset=list(range(1,n+1))
 
 def get_subsets(fullset):
     listrep = list(fullset)
@@ -21,35 +22,98 @@ def get_subsets(fullset):
                 subset.append(listrep[k])
         subsets.append(subset)
     return subsets
-subsets = get_subsets(set([1,2,3,4]))
-print("The power set in order is", subsets)
+Pn = get_subsets(set(nset))
+print("The power set Pn is:\n", Pn, "\n")
 
-something=[] # ask sam if there is a easier way to do this in general.
-for i in subsets:
-    if len(i)==1: # i want to interate through the length of subsets.
-        something.append(i)
-for i in subsets:
-    if len(i)==2:
-        something.append(i)
-for i in subsets:
-    if len(i)==3:
-        something.append(i)
-for i in subsets:
-    if len(i)==4:
-        something.append(i)
-for i in subsets:
-    if len(i)==5:
-        something.append(i)
-Pn = something
-print("Reordering by length gives us: ", Pn, "\n")
+#########: Finished: This states the subsets of length r ###############
+empty3=[]
+for j in range(1,len(nset)):
+    empty = []
+    for i in iter.distinct_combinations((nset),j):
+        empty.append(i)
+    empty = [list(k) for k in empty ]#list tuple. #change eah tuple to list.Done
+    print("The",j,"-sets of", nset,"are: \n",empty, "\n")
+    empty3.extend(empty) #extend adds th elements not the list as append does.
+empty3.append(nset)
+print("Reordering Pn by length and removing [] returns: \n", empty3, "\n")
+#########: Finished: Obj: to give a list of all disjoint unions in Pn ###########
 
-empty = []
-for i in iter.distinct_combinations(([1,2,3]),2):
-    empty.append(i)
-print(empty)
+# Function to convert list to a string with spaces.
+def listToString(s):
+    # initialize an empty string
+    str1 = " "
+    # return string
+    return (str1.join(s))
 
-iterable = "1234"
+# Driver code
+l_st_nset= [str(item) for item in nset]
+st_nset_space=(listToString(l_st_nset))
+st_nset=st_nset_space.replace(' ','')
+#print(st_nset)
+
+iterable = st_nset #string version of nset
+empty2=[]
 for part in set_partitions(iterable, 2):
-    print([''.join(p) for p in part])
-    #branchtest
-#test
+    s1=''
+    x=([s1.join(p) for p in part]) # How do store this
+    # Convert string element back to list e.g [[1],[2,3,4]]
+    # want to put these into list. currently in form x=['1', '2345']
+    y = [list((word)) for word in x]
+    #print(y)
+    empty2.append([[int(i) for i in j] for j in y])# want to store all and put all into list.
+djpair=empty2
+print("The disjoint unions pairs of", nset,"are: \n",djpair)
+
+### Attempt 1 at counting functions
+
+# need a choice of base values for 2 -sets. e.g
+#example seed material.
+
+# func_s1([i])=0
+# func_s2([1, 2]) = 1
+# func_s2([1, 3])=0
+# func_s2([2, 3])=0
+
+
+#Set Pn as the powerset ordered by length.
+
+OneSets0= [x for x in Pn if len(x)==1]
+print(OneSets0)
+dictempty={}
+for i in OneSets0:
+    dictempty[tuple(i)]=0
+print(dictempty)
+
+# def twosetfunction0(twoset): #assigns a choice of 2sets the value 1.
+#     return {twoset:0}
+# def twosetfunction1(twoset): #assigns a choice of 2sets the value 1.
+#     return {twoset:1}
+#Dictionaries store: string,number or tuple as keys and value can be anything.
+#must change list to tuple before storage.
+# L=tuple([1,3])
+# make a choice for 2 sets with 0 and those with value 1.
+# TwoSets0 = # x needs to be a tuple,string,
+# TwoSets1 =
+# #pass respectively through twosetfunction0 & twosetfunction1.
+# value = twosetfunction1(L)
+# TwoSetsValue0= {x: 0 for i in TwoSets0} # x needs to be a tuple,string,
+# TwoSetsValue1= {x: 1 for i in TwoSets1}
+# print(Merge(TwoSetsValue0,TwoSetsValue1))
+#
+
+
+#
+#
+# p1 = djpair[0] #  test: set p as first djpair
+# djpair_funcval_0(p[0],p[1])
+# def djpair_funcval_0(element1,element2): #for each element of djpair list.
+# #takes in pair in djpair, outputs sum f(I)+f(J)
+# def djpair_funcval_1
+#
+#
+#
+# # def msa2_function(element): #For subset I irrespective of decomp.
+# def Merge(dict1, dict2):
+#     return(dict2.update(dict1))
+#
+#
---
+++ Tutorials_Examples/NumpyTrial.py
@@ -0,0 +1,36 @@
+import numpy as np
+# x=np.zeros((2,3), dtype = int) # array with shape of zeros
+# print(x)
+#
+# y=np.ones((4,5))
+# print(y)
+#
+# x=np.arange(10)
+# print(x)
+# x=np.arange(5,10)
+# print(x)
+# x=np.arange(1,11,2)
+# print(x)
+# x=np.linspace(1.,4.,6) # what do we have to co up by to go from 1 to 4.
+# print(x)
+# x=np.full((2,2),8)
+# print(x)
+# x=np.eye(5)
+# print(x)
+#
+# arr = np.array([[1,2,3],[4,5,6]]) # looks like matrix
+# li = [[1,2,3],[4,5,6]]
+# print(arr)
+# print(li)
+# element = arr[0,1] #row 0 column 1
+# print(element)
+# print(arr.shape) # rows and colums
+# print(arr.size) # total number of elements
+# arr2 = np.array([[1,2,3],[1,2,3]]) # not linear alg dim
+# print(arr.ndim)
+# #How to append
+# arr3=np.append(arr2,99)
+# print(arr3)
+# #how to delete
+# arr4 = np.delete(arr3,1)
+# print(arr4)
